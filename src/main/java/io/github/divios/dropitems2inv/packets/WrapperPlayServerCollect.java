0bb7e4ec462260e8a32db8d1be7dfe70dd27cbaf
Divios <serberpar@gmail.com>
2021-01-21 17:03:52 +0100

    new packet handlers

---
+++ src/main/java/io/github/divios/dropitems2inv/packets/WrapperPlayServerCollect.java
@@ -0,0 +1,55 @@
+package io.github.divios.dropitems2inv.packets;
+
+import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.events.PacketContainer;
+import io.github.divios.dropitems2inv.packets.AbstractPacket;
+
+public class WrapperPlayServerCollect extends AbstractPacket {
+    public static final PacketType TYPE = PacketType.Play.Server.COLLECT;
+
+    public WrapperPlayServerCollect() {
+        super(new PacketContainer(TYPE), TYPE);
+        handle.getModifier().writeDefaults();
+    }
+
+    public WrapperPlayServerCollect(PacketContainer packet) {
+        super(packet, TYPE);
+    }
+
+    /**
+     * Retrieve Collected Entity ID.
+     *
+     * @return The current Collected Entity ID
+     */
+    public int getCollectedEntityId() {
+        return handle.getIntegers().read(0);
+    }
+
+    /**
+     * Set Collected Entity ID.
+     *
+     * @param value - new value.
+     */
+    public void setCollectedEntityId(int value) {
+        handle.getIntegers().write(0, value);
+    }
+
+    /**
+     * Retrieve Collector Entity ID.
+     *
+     * @return The current Collector Entity ID
+     */
+    public int getCollectorEntityId() {
+        return handle.getIntegers().read(1);
+    }
+
+    /**
+     * Set Collector Entity ID.
+     *
+     * @param value - new value.
+     */
+    public void setCollectorEntityId(int value) {
+        handle.getIntegers().write(1, value);
+    }
+
+}
