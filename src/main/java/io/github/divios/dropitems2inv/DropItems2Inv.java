d7138932052632328f8e3ff16e0376f5b23a4f87
Divios <serberpar@gmail.com>
2021-01-21 17:04:20 +0100

    added protocolmanager support

--- src/main/java/io/github/divios/dropitems2inv/DropItems2Inv.java
+++ src/main/java/io/github/divios/dropitems2inv/DropItems2Inv.java
@@ -1,14 +1,18 @@
 package io.github.divios.dropitems2inv;
 
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.ProtocolManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class DropItems2Inv extends JavaPlugin {
 
     private static DropItems2Inv instance;
     private static boolean enable = true;
+    private static ProtocolManager protocolManager = null;
 
     @Override
     public void onEnable() {
+
         instance = this;
         listeners.getInstance();
 
@@ -26,6 +30,10 @@ public final class DropItems2Inv extends JavaPlugin {
 
     public static boolean isEnabledv() {
         return enable;
+    }
+
+    public static ProtocolManager getProtocolManager() {
+        return protocolManager;
     }
 
     public static void toggleEnable() {
