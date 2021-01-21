8072ddc4164287a97109ccd835dfd142361f0037
Divios <serberpar@gmail.com>
2021-01-21 17:06:58 +0100

    added protocolmanager support

--- src/main/java/io/github/divios/dropitems2inv/listeners.java
+++ src/main/java/io/github/divios/dropitems2inv/listeners.java
@@ -1,24 +1,35 @@
 package io.github.divios.dropitems2inv;
 
+import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.ProtocolManager;
+import com.comphenix.protocol.events.PacketContainer;
+import io.github.divios.dropitems2inv.packets.WrapperPlayServerCollect;
 import org.bukkit.Bukkit;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityPickupItemEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.inventory.ItemStack;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class listeners implements Listener {
 
     private static listeners Listeners = null;
-
+    private static ProtocolManager protocolManager;
+    private static DropItems2Inv main;
 
     private static void init() {
         Listeners = new listeners();
+        main = DropItems2Inv.getInstance();
+        if(main.getServer().getPluginManager().getPlugin("ProtocolLib") != null) {
+            protocolManager = ProtocolLibrary.getProtocolManager();
+        }
         Bukkit.getPluginManager().registerEvents(getInstance(), DropItems2Inv.getInstance());
-
     }
 
     public static listeners getInstance() {
@@ -69,11 +80,20 @@ public class listeners implements Listener {
         }
         p.getInventory().setItem(slot, utils.removeMetadata(item.clone()));
 
-        /*
         e.setCancelled(true);
+        if(protocolManager != null) {
+            WrapperPlayServerCollect wpsc = new WrapperPlayServerCollect();
+            wpsc.setCollectedEntityId(e.getItem().getEntityId());
+            wpsc.setCollectorEntityId(p.getEntityId());
+            wpsc.sendPacket(p);
+
+        }
         e.getItem().remove();
-        */
-        utils.destroyItem(p, utils.firstEmpty(p.getInventory()));
+
+        try{
+            p.playSound(p.getLocation(), Sound.ENTITY_ITEM_PICKUP, 0.5F, 1.5F);
+        } catch (NoSuchFieldError ignored) {}
+        //utils.destroyItem(p, utils.firstEmpty(p.getInventory()));
 
     }
 
